20ea645c3fa9353b06431411a237d82a08cdde14
Hex-a <mc.pires@campus.fct.unl.pt>
2017-12-05 11:27:53 +0000

    move solved puzzles into proper directories indexed by day

---
+++ day1/inverse_captcha.scala
@@ -0,0 +1,8 @@
+val isPart2 = args.size > 0 && args(0) == "part2"
+val ln: Array[Int] = readLine().split("").map(_.toInt).toArray
+val step = if (isPart2) ln.size/2 else 1
+val result = ln.zipWithIndex
+    .filter { case (c, i) => ln((i + step) % ln.size) == c }
+    .foldLeft(0)(_+_._1)
+
+println(result)
